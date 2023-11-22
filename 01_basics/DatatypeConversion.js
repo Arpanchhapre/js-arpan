6b63b36e0da219237ab83417f3c5da2268ad04b1
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-11-22 09:33:58 +0000

    conversionOfDAtatypes

---
+++ 01_basics/DatatypeConversion.js
@@ -0,0 +1,25 @@
+let score = null
+
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+
+let isLoggedIn = "arpan"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1=> true 0=> false
+// "" => false
+//"hitesh" => true
+
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber)
+console.log(typeof StringNumber)
