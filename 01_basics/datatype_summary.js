c16128b24981546b4e9c18d53b46087f7f2516d3
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-11-22 17:56:59 +0000

    datatypeSummary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,43 @@
+// #primitive datatype => 
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt 
+
+
+// # Refference (Non-primitive)
+//Arrays , Objects , 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail = null;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 8236413874908134n;
+
+console.log(bigNumber);
+
+const heros = ["shaktiman ", "naagraj","doga"]
+console.log(heros);
+
+let myObj = {
+    name : "arpan",
+    age : 22,
+}
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("hello Arpan");
+}
+
+console.log(myFunction)
+
+console.log(typeof myFunction)  //Function Object
+console.log(typeof Symbol)
+
