7f1c9d1455baebed5596fea737979cef8bfcadd3
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-11-22 09:16:20 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3);// we are using not js not browser
+
+console.log(3
+    +3) // code reusability should be high
+
+console.log("arpan")
+
+
+let name = "arpan" // String 
+
+let age = 12 // number
+
+let isLoggedIn = true // boolean
+
+// null :- standalone value or representation of empty value
+// undefined :- 
+// symbol :- uniqueness 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof nu4); // object
+ 
