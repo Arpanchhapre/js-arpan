2085bd100aedaa9bd6b2b378ed88ca58efb167d3
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-11-26 18:33:36 +0000

    03_objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -35,4 +35,9 @@ JsUser.greeting = function(){
 
 console.log(JsUser.greeting())
 
-JsUser.g
+JsUser.greetingTwo = function(){
+    console.log(`hello JS user , ${this.name}`)
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
