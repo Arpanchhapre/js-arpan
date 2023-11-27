37d730a4881359375625a5a8904e770487c877f2
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-11-27 07:34:58 +0000

    05_objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor : instructor} =   // object destructuring
+
+console.log(instructor) 
+
+// {
+//   "name" : "hitesh",
+//   "coursename" : "js in hindi",
+//   "price" : "free"
+// }
+// json format and API's
+[
+    {},
+    {},
+    {}
+]
