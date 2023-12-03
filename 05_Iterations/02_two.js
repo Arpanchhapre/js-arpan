7e3da8fc262b14698308a4d0981cf0ab9ecd9693
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-12-03 09:48:02 +0000

    two

---
+++ 05_Iterations/02_two.js
@@ -0,0 +1,19 @@
+let i = 0
+while(i<=12)
+{
+    console.log(i)
+    i++;
+}
+
+let myArr = ["flash" , "shaktiman" , "arpan"]
+let arr = 0;
+while(arr<myArr.length){
+console.log(myArr[arr]);
+arr = arr + 1;
+}
+
+let score = 1
+do{
+    console.log(score)
+    score++
+}while(score <= 15) // in do while loop kaam pehle hota h condition baad m check hoti hai
