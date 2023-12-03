939210eb17af31adfa860e5d1535b92408a9c4af
Arpanchhapre <88194031+Arpanchhapre@users.noreply.github.com>
2023-12-03 09:50:16 +0000

    five

---
+++ 05_Iterations/05_five.js
@@ -0,0 +1,33 @@
+const coding = ["js","ruby","java","python"]
+
+coding.forEach( function (item){
+    console.log(item)
+} )
+
+coding.forEach((item) => {
+    console.log(item)
+})
+
+// function printme(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printme)
+
+coding.forEach((item,index ,arr) =>{
+  console.log(item,index,arr)
+})
+
+const myCoding = [{
+    languageName : "javascript",
+    languageFileName : "js"
+},
+{
+    languageName: "python",
+    languageFileName : "ps"
+},
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName)
+})
